72d62b3c55dfe400e72987bd399b5a4464822c36
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2021-12-24 11:15:52 +0900

    store add

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "redux";
+
+const initialState = {
+  count: 1,
+}
+const reducer = (state = initialState) => {
+  return state;
+}
+
+const store = createStore(reducer);
+
+export default store;
